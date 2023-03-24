17db600c609d92e99ee3a954e9dab02d20e6d877
Bruno Athie Teruel <bat223@uky.edu>
2023-03-23 22:56:34 -0400

    Added backbones of website and navbars

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate
+} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>
+        Hi, my name is Bruno Athie Teruel
+      </h1>
+      <nav className="nav" id="nav">
+        <Link to="/work" class="navbar-item">Work</Link>
+        <Link to="/about" class="navbar-item">About</Link>
+        <Link to="/contact" class="navbar-item">Contact</Link>
+      </nav>
+    </div>
+  );
+}
+
+function Work() {
+  return (
+    <p> work </p>
+  );
+}
+
+function About() {
+  return (
+    <p> about </p>
+  );
+}
+
+function Contact() {
+  return (
+    <p> contact </p>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="page" style={{marginLeft: 30, marginRight: 30}}>
+        <Header/>
+        <div class="main-page">
+          <Routes>
+            <Route path="/work" element={<Work/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/contact" element={<Contact/>}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
