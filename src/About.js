9d8e87f04cc7b0d9b2a3b373af505ce34b6e0334
Bruno Athie Teruel <bat223@uky.edu>
2023-04-17 21:36:32 -0400

    Added src files

---
+++ src/About.js
@@ -0,0 +1,38 @@
+import './Page.scss';
+
+export function About() {
+  return (
+    <div className='box'>
+      <img src={require("./assets/bruno-archaeology.jpg")} alt="" className='self-img'/>
+      <p>
+        I was born in Avaré, Brazil. In 2019, I got a full ride to study
+        at the University of Kentucky, where I completed a dual degree in
+        Computer Science and Anthropology, and won awards for academic
+        excellence in both. During my time in the USA, I worked in the
+        interesection between computer science and the humanities, and 
+        found a new passion in the interdisciplinary gray areas that 
+        separate disciplines. I am also passionate about the arts and
+        design.
+      </p>
+      <p>
+        I worked as an intern in both research and industry, with a focus 
+        on computer vision and 3D computer graphics. For two years I worked 
+        at EduceLab, where we employed CT scanning, computer vision, and 
+        machine learning to virtually unwrap damaged historical documents.
+        I was also a researcher at the Human Evolution and Virtual Archaeology
+        Lab, where I employed CT scanning, Image segmentation, landmark-based
+        analysis, and again machine learning to study human remains from
+        archaological sites in Brazil and Italy. Through technology I 
+        contributed to the study of valuable artifacts of cultural heritage 
+        non-invasively. I have presented my research in Italy, Wisconsin, 
+        Nevada, and Kentucky.
+      </p>
+      <p>
+        email: bathieteruel@gmail.com
+      </p>
+      <p>
+        phone: +1 (859) 539-2771
+      </p>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,81 @@
+import './App.scss';
+import { Background } from './Background';
+import { Work } from './Work';
+import { About } from './About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+function Header(props) {
+  const location = useLocation();
+  const [isHeaderSmall, setIsHeaderSmall] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname === '/about' || location.pathname === '/work' || location.pathname === '/art') {
+      setIsHeaderSmall(true);
+    } else {
+      setIsHeaderSmall(false); 
+    }
+  }, [location.pathname]);
+
+
+  const classname = `header2 ${isHeaderSmall ? 'small' : ''}`
+  const headerTextName = `header-text ${isHeaderSmall ? 'hidden' : ''}`
+  
+  return (
+    <div className={classname}>
+      <div className='header-background'></div>
+      <div className='NavPhrase'>
+        <nav className="nav" id="nav">
+          <Link to="/about" class="navbar-item">about</Link>
+          <Link to="/work" class="navbar-item">experience</Link>
+          <Link to="/art" class="navbar-item">art</Link>
+        </nav>
+      </div>
+      <div className='name-block'>
+        <Link className="name" to="/">
+          Bruno<br/>
+          Athié<br/>
+          Teruel<br/>
+        </Link>
+        <p className={headerTextName}>computer<br/>scientist<br/>&<br/>anthropologist</p>
+      </div>
+    </div>
+  );
+}
+
+function Art() {
+  return (
+    <div>
+      <div className="block-page">
+        <p>art will go here (page under construction)</p>
+      </div>
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <Router>
+      <Background/>
+      <div className="page" style={{marginLeft: 10, marginRight: 10}}>
+        <Header/>
+        <div>
+          <Routes>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/work" element={<Work/>}></Route>
+            <Route path="/art" element={<Art/>}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
